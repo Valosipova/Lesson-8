241980ef6e636af50e49dd4bd1fd3817d9a39b02
Valentina <valush31@gmail.com>
2023-03-26 17:30:46 +0300

    Task 54

---
+++ Task_54/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, 
+//которая упорядочит по убыванию элементы каждой строки двумерного массива.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//В итоге получается вот такой массив:
+//7 4 2 1
+//9 5 3 2
+//8 4 4 2
+
+void CreateRandomArray(int[,] arr)
+{
+  for (int i = 0; i < arr.GetLength(0); i++)
+    {
+      for (int j = 0; j < arr.GetLength(1); j++)
+      {
+        arr[i, j] = new Random().Next(1, 100);
+      }
+    }
+}
+
+void ShowArray(int[,] arr)
+{
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      Console.Write($"{arr[i, j]} ");
+    }
+      Console.WriteLine();
+  }
+}
+
+void DescendingArrayNumbers(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+const int numberOfLines = 5;
+const int numberOfColumns = 4;
+int[,] array = new int[numberOfLines, numberOfColumns];
+
+CreateRandomArray(array);
+ShowArray(array);
+
+Console.WriteLine($"Новый массив: ");
+DescendingArrayNumbers(array);
+ShowArray(array);
